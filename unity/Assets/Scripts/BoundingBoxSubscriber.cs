16cccc427450183e7681620861af6abccea70419
ericx1107 <sxuu0041@student.monash.edu>
2022-02-01 15:17:51 +1100

    Added bounding boxes display in Unity

---
+++ unity/Assets/Scripts/BoundingBoxSubscriber.cs
@@ -0,0 +1,72 @@
+using RosMessageTypes.JskRecognition;
+using RosMessageTypes.Geometry;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Robotics.ROSTCPConnector;
+using Unity.Robotics.ROSTCPConnector.ROSGeometry;
+using UnityEngine;
+
+public class BoundingBoxSubscriber : MonoBehaviour
+{
+    [SerializeField]
+    private string boxTopic = "/segmentation/bounding_boxes";
+
+    private ROSConnection rosConnection;
+
+    public void Start()
+    {
+        // set up ros subscriber
+        rosConnection = ROSConnection.GetOrCreateInstance();
+        rosConnection.Subscribe<BoundingBoxArrayMsg>(boxTopic, BoundingBoxesCallback);
+    }
+
+    private void BoundingBoxesCallback(BoundingBoxArrayMsg message)
+    {
+        // Debug.Log(message);
+        // destroy previous exisiting bounding boxes
+        GameObject[] oldBoxes = GameObject.FindGameObjectsWithTag("Bounding Box");
+        foreach(GameObject oldBox in oldBoxes)
+        {
+            GameObject.Destroy(oldBox);
+        }
+
+        // get coordinates from message for each box
+        foreach(var box in message.boxes)
+        {
+            // Debug.Log(box);
+
+            var position = box.pose.position.From<FLU>();
+            var orientation = box.pose.orientation.From<FLU>(); // quaternion
+            var dimensions = box.dimensions.From<FLU>();
+
+            // draw to unity with gizmos
+
+            // create cube object in unity
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.tag = "Bounding Box";
+            cube.transform.position = position;
+            cube.transform.rotation = orientation;
+            cube.transform.localScale = dimensions;
+
+            // make cube transparent
+            Material mat = cube.GetComponent<Renderer>().material;
+            float alpha = 0.5f;
+            Color transparentColour = new Color(0.6f, 0.6f, 0.6f, alpha);//mat.color.r, mat.color.g, mat.color.b, alpha);
+            mat.SetColor("_Color", transparentColour);
+            mat.shader = Shader.Find("Transparent/Diffuse");
+        }
+    }
+
+    // public void Update()
+    // {
+    //     GameObject[] boxes = GameObject.FindGameObjectsWithTag("Bounding Box");
+    //     foreach(GameObject box in boxes)
+    //     {
+    //         Material mat = box.GetComponent<Renderer>().material;
+    //         float alpha = 0.5f;
+    //         Color transparentColour = new Color(1,1,1,alpha);//mat.color.r, mat.color.g, mat.color.b, alpha);
+    //         mat.SetColor("_Color", transparentColour);
+    //     }
+    // }
+
+}
