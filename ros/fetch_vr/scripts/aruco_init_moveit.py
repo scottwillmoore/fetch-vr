19f3a9fd83c7e95122831cff30fe1321cb53a74a
eric <xushiyu1107@gmail.com>
2022-02-10 12:32:26 +1100

    Implemented initialisation of moveit planning scene and robot pose

---
+++ ros/fetch_vr/scripts/aruco_init_moveit.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+
+import sys
+import rospy
+import moveit_commander
+import copy
+
+from aruco_msgs.msg import MarkerArray
+from geometry_msgs.msg import PoseStamped
+
+class ArucoInit():
+    
+    def __init__(self):
+        self.scene = moveit_commander.PlanningSceneInterface(synchronous=True)
+
+        # table size: w80cm l180cm h77cm
+        self.table_size = [0.82, 1.80, 0.85]
+        # obstructions position: 64cm to the right, 42cm to the back
+        self.obstruction_side_offset = 0.64
+        self.obstruction_back_offset = 0.40
+
+        # self.pcd_subscriber = rospy.Subscriber(
+        #     '/aruco_marker_publisher/markers',
+        #     aruco_msgs.msg.MarkerArray,
+        #     self.add_object_to_scene,
+        #     queue_size=1
+        #     )
+
+        # self.robot = moveit_commander.RobotCommander()
+
+        self.arm = moveit_commander.MoveGroupCommander("arm")
+
+    def init_moveit(self, aruco_array):
+        for marker in aruco_array.markers:
+            if marker.id == 201:    # marker on the right
+                marker_pose = marker.pose.pose.position
+                break
+            
+        try:
+            marker_pose
+        except NameError:
+            sys.exit("\n\n\nCannot see marker!\n\n\n")
+        
+        box_pose = PoseStamped()
+        box_pose.header.frame_id = "base_link"
+        box_pose.pose.position = marker_pose
+        # marker is at top of table, box position needs to be at the centre
+        # left marker is 38cm in -x direction 12 in -y direction from the centre of the table
+        box_pose.pose.position.x = box_pose.pose.position.x + .33
+        box_pose.pose.position.y = box_pose.pose.position.y + .12
+        box_pose.pose.position.z = box_pose.pose.position.z - .38 # self.table_size[2]/2
+        box_name = "table"
+        self.scene.add_box(box_name, box_pose, size=self.table_size)
+        
+        right_obst_pose = copy.deepcopy(box_pose)
+        right_obst_pose.pose.position.y = right_obst_pose.pose.position.y - self.obstruction_side_offset
+        self.scene.add_box("right_obstruction", right_obst_pose, size=(2, 0.01, 4))
+
+        left_obst_pose = copy.deepcopy(box_pose)
+        left_obst_pose.pose.position.y = left_obst_pose.pose.position.y + self.obstruction_side_offset
+        self.scene.add_box("left_obstruction", left_obst_pose, size=(2, 0.01, 4))
+        
+        back_obst_pose = copy.deepcopy(box_pose)
+        back_obst_pose.pose.position.x = back_obst_pose.pose.position.x + self.obstruction_back_offset
+        self.scene.add_box("back_obstruction", back_obst_pose, size=(0.01, 2, 4))
+
+        # while not rospy.is_shutdown():
+        #     attached_objects = self.scene.get_attached_objects([box_name])
+        #     is_attached = len(attached_objects.keys()) > 0
+
+        #     is_known = box_name in self.scene.get_known_object_names()
+
+        #     if (is_attached) and (is_known):
+        #         return
+        
+        self.scene.add_box("temp_box", box_pose, size=(self.table_size[0], self.table_size[1], self.table_size[2] + 1))
+
+        # print(self.arm.get_joints())
+        # print((self.arm.get_current_joint_values()))
+
+        start_pose = [-0.6189127329589844, -0.5323255894348145, -0.7583010278918763, -1.276721908996582, -2.165996028260498, -1.2340825009521483, -2.4642663503967284]
+
+        # self.arm.plan(start_pose)
+
+        self.arm.go(start_pose, wait=True)
+        self.arm.stop()
+
+        self.scene.remove_world_object("temp_box")
+
+    
+        
+
+#name: [l_wheel_joint, r_wheel_joint, torso_lift_joint, bellows_joint, head_pan_joint, head_tilt_joint, shoulder_pan_joint, shoulder_lift_joint, upperarm_roll_joint, elbow_flex_joint, forearm_roll_joint, wrist_flex_joint, wrist_roll_joint]
+#position: [0.2015855312347412, 6.142337322235107, 0.19760264456272125, 0.092, -0.006723165512084961, 0.3862792734008789, -0.6189127329589844, -0.5323255894348145, -0.7583010278918763, -1.276721908996582, -2.165996028260498, -1.2340825009521483, -2.4642663503967284]
+#arm joints: ['shoulder_pan_joint', 'shoulder_lift_joint', 'upperarm_roll_joint', 'elbow_flex_joint', 'forearm_roll_joint', 'wrist_flex_joint', 'wrist_roll_joint']
+
+#tucked joint states:[1.3211896058319093, 1.400107062084961, -0.19763138222480012, 1.7191431975860596, -1.511132454752942e-05, 1.660155971032715, 7.333383261561369e-05]
+
+if __name__ == '__main__':
+    try:
+        rospy.init_node('add_aruco_object_to_scene')
+        moveit_commander.roscpp_initialize(sys.argv)
+        aruco_array = rospy.wait_for_message('/aruco_marker_publisher/markers', MarkerArray)
+        
+        box = ArucoInit()
+
+        box.init_moveit(aruco_array)
+
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
--- ros/fetch_vr/scripts/segmentation.py
+++ ros/fetch_vr/scripts/segmentation.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
+from sklearn.utils import compute_class_weight
 import rospy
 import open3d as o3d
 import numpy as np
 import scipy
+import time
 
 from sensor_msgs.msg import PointCloud2
 from geometry_msgs.msg import Point, Quaternion, Pose, Vector3
@@ -15,6 +17,7 @@ class Segmentation():
 
     def __init__(self):
         self.input_cloud_topic = rospy.get_param('~input_cloud_topic')
+        self.compute_bounding_boxes = rospy.get_param('~compute_bounding_boxes')
 
         self.point_cloud_listener = rospy.Subscriber(
             self.input_cloud_topic,
@@ -40,22 +43,43 @@ class Segmentation():
 
     def callback(self, pcd_ros):
 
+        start_time = time.time()
+
         pcd_frame = pcd_ros.header.frame_id
         # print(pcd_frame)
 
         pcd = rospc_to_o3dpc(pcd_ros)
+        print('src:', pcd)
 
-        # Run RANSAC to detect largest plane, assume this is the table
-        plane_threshold = 5000
+        o3d.io.write_point_cloud("raw.pcd", pcd, print_progress=True)
 
-        while True:
-            plane_model, inliers = pcd.segment_plane(distance_threshold=0.01,ransac_n=3,num_iterations=100)
-            [a, b, c, d] = plane_model
+        # Remove points further away than this threshold
+        distance_threshold = 1.2
+
+        #norms = np.linalg.norm(np.asarray(pcd.points), axis=np.argmin(np.asarray(pcd.points).shape))
+        dist = np.asarray(pcd.points)[:, 0]
+
+        pcd = pcd.select_by_index([i for i in range(len(dist)) if dist[i] < distance_threshold])
+        print('remove far points:', pcd)
+
+        # Remove points lower than this threshold
+        height_threshold = 0.8
+
+        heights = np.asarray(pcd.points)[:, 2]
+
+        pcd = pcd.select_by_index([i for i in range(len(heights)) if heights[i] > height_threshold])
+        print('remove low points:', pcd)
+
+        # Remove planes with more points than this threshold
+        plane_threshold = 10000
+
+        for i in range(5):
+            plane_model, inliers = pcd.segment_plane(distance_threshold=0.005,ransac_n=3,num_iterations=50)
             plane_cloud = pcd.select_by_index(inliers)         # plane
             plane_size = len(plane_cloud.points)
 
             if plane_size < plane_threshold:
-                break
+                continue
             
             
             pcd = pcd.select_by_index(inliers, invert=True)       # rest of the objects
@@ -63,57 +87,64 @@ class Segmentation():
             # print(f"Plane equation: {a:.2f}x + {b:.2f}y + {c:.2f}z + {d:.2f} = 0")
             print(f"Removed plane with {plane_size} points")
         
-        # Run DBscan on the rest of the point cloud to segment the objects on the table
-        labels = np.array(pcd.cluster_dbscan(eps=0.05, min_points=500))
-        max_label = labels.max()
-        print(f"{max_label + 1} objects detected in point cloud")
-
-        # Get bounding boxes for each segmented object
-        # Initialise jsk BBArray message
-        bbox_array = BoundingBoxArray()
-        bbox_array.header.frame_id = pcd_frame
-        bbox_array.header.stamp = rospy.Time.now()
-
-        # Loop through each segmented object
-        for i in range(max_label + 1):
-            
-            # Obtain bounding box for the object
-            cluster = o3d.geometry.PointCloud()
-            cluster.points = o3d.utility.Vector3dVector(np.asarray(pcd.points)[np.where(labels==i)])
-
-            bbox = cluster.get_oriented_bounding_box()
-
-            # Construct bounding box message
-            bbox_msg = BoundingBox()
-            bbox_msg.header.frame_id = pcd_frame
-            bbox_msg.header.stamp = rospy.Time.now()
-            
-            # open3d rotation matrices can have -1 det, improper roation with reflections
-            # scipy only works with +1 det, will convert the matrix
-            # inverting each element will change det from -1 to +1
-            if np.linalg.det(bbox.R) > 0:
-                quat = scipy.spatial.transform.Rotation.from_matrix(bbox.R).as_quat()
-            else:
-                quat = scipy.spatial.transform.Rotation.from_matrix(-bbox.R).as_quat()
-            
-            bbox_msg.pose = Pose(Point(*bbox.center), Quaternion(*quat))
-
-            bbox_msg.dimensions = Vector3(*bbox.extent)
-
-            # Add to array
-            bbox_array.boxes.append(bbox_msg)
+        # Clean up point cloud
+        #pcd, ind = pcd.remove_radius_outlier(nb_points=500, radius=0.04)
+        print('remove radius outliers:', pcd)
         
-        # print(f"point cloud has {max_label + 1} clusters")
+        if self.compute_bounding_boxes:
+            # Run DBscan on the rest of the point cloud to segment the objects on the table
+            labels = np.array(pcd.cluster_dbscan(eps=0.04, min_points=500))
+            max_label = labels.max()
+            print(f"{max_label + 1} objects detected in point cloud")
 
-        # Publish segment bounding boxes
-        self.bounding_box_publisher.publish(bbox_array)
-        print("Publishing bounding boxes...")
+            # Get bounding boxes for each segmented object
+            # Initialise jsk BBArray message
+            bbox_array = BoundingBoxArray()
+            bbox_array.header.frame_id = pcd_frame
+            bbox_array.header.stamp = rospy.Time.now()
+
+            # Loop through each segmented object
+            for i in range(max_label + 1):
+                
+                # Obtain bounding box for the object
+                cluster = o3d.geometry.PointCloud()
+                cluster.points = o3d.utility.Vector3dVector(np.asarray(pcd.points)[np.where(labels==i)])
+
+                bbox = cluster.get_oriented_bounding_box()
+
+                # Construct bounding box message
+                bbox_msg = BoundingBox()
+                bbox_msg.header.frame_id = pcd_frame
+                bbox_msg.header.stamp = rospy.Time.now()
+                
+                # open3d rotation matrices can have -1 det, improper roation with reflections
+                # scipy only works with +1 det, will convert the matrix
+                # inverting each element will change det from -1 to +1
+                if np.linalg.det(bbox.R) > 0:
+                    quat = scipy.spatial.transform.Rotation.from_matrix(bbox.R).as_quat()
+                else:
+                    quat = scipy.spatial.transform.Rotation.from_matrix(-bbox.R).as_quat()
+                
+                bbox_msg.pose = Pose(Point(*bbox.center), Quaternion(*quat))
+
+                bbox_msg.dimensions = Vector3(*bbox.extent)
+
+                # Add to array
+                bbox_array.boxes.append(bbox_msg)
+            
+            # print(f"point cloud has {max_label + 1} clusters")
+
+            # Publish segment bounding boxes
+            self.bounding_box_publisher.publish(bbox_array)
+            print("Publishing bounding boxes...")
 
         # Publish segmented point cloud
         pcd_msg = o3dpc_to_rospc(pcd, frame_id=pcd_frame)
 
         self.point_cloud_publisher.publish(pcd_msg)
         print("Publishing objects point cloud...")
+
+        print(f"Time taken: {time.time() - start_time}")
 
 if __name__ == "__main__":
 
